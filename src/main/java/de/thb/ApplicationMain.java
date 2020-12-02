8f74f131b6a3aa28e51f53f4f92fd464b1ac3e5f
Mistra Forest Kuipou Tchiendja <forest@cantaa.de>
2020-12-03 00:51:42 +0100

    UI property refined, event details better designed, EventPropertyUI.java and EventUtilities have been created for this

--- src/main/java/de/thb/ApplicationMain.java
+++ src/main/java/de/thb/ApplicationMain.java
@@ -1,14 +1,27 @@
 package de.thb;
 
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.EtchedBorder;
+import de.thb.view.EventPropertyUI;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JList;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.DefaultListModel;
 import javax.swing.table.TableCellRenderer;
-import java.awt.*;
+import java.awt.GridLayout;
+import java.awt.Component;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
 import java.awt.event.KeyEvent;
 import java.util.Vector;
 
 public class ApplicationMain extends JPanel {
+
     public ApplicationMain() {
         super(new GridLayout(1, 1));
 
@@ -19,21 +32,7 @@ public class ApplicationMain extends JPanel {
                 "Booked tickets",
                 "Actual availability"};
 
-
-//        Object[][] data = {
-//                {"Kathy", "Smith",
-//                        "Snowboarding", new Integer(5), new Boolean(false)},
-//                {"John", "Doe",
-//                        "Rowing", new Integer(3), new Boolean(true)},
-//                {"Sue", "Black",
-//                        "Knitting", new Integer(2), new Boolean(false)},
-//                {"Jane", "White",
-//                        "Speed reading", new Integer(20), new Boolean(true)},
-//                {"Joe", "Brown",
-//                        "Pool", new Integer(10), new Boolean(false)}
-//        };
-
-
+//        Define rows and columns for second view (table view)
         Vector<String> columnNames = new Vector<String>();
         for (String name : columnNamesItem) {
             columnNames.addElement(name);
@@ -54,13 +53,14 @@ public class ApplicationMain extends JPanel {
         rowData.addElement(rowOne);
         rowData.addElement(rowTwo);
 
-
+        //Define the tab component to hold two views
         JTabbedPane tabbedPaneViewChooser = new JTabbedPane();
+
+        //Components for the first view
         JPanel eventPanel = new JPanel();
         JPanel eventTablePanel = new JPanel();
-        JPanel eventDetailPanel = new JPanel();
-        eventPanel.setLayout(new GridLayout(0, 2, 5, 10));
-        eventPanel.setBounds(20,20,200,300);
+        eventPanel.setLayout(new GridLayout(0, 1, 5, 10));
+        eventPanel.setBounds(20,20,60,300);
         JScrollPane scrollPaneForEvents = new JScrollPane();
 
         JTable table = new JTable(rowData, columnNames){
@@ -71,7 +71,6 @@ public class ApplicationMain extends JPanel {
                 if (!returnComp.getBackground().equals(getSelectionBackground())) {
                     Color bg = (row % 2 == 0 ? alternateColor : whiteColor);
                     returnComp.setBackground(bg);
-                    bg = null;
                 }
                 return returnComp;
             }
@@ -83,46 +82,7 @@ public class ApplicationMain extends JPanel {
 
         //Create the scroll pane and add the table to it.
         JScrollPane scrollPaneForTable = new JScrollPane(table);
-//Add the scroll pane to this panel.
         eventTablePanel.add(scrollPaneForTable);
-
-
-        JTextField eventNameField = new JTextField(10);
-        JLabel eventNameLabel = new JLabel("  Event name");
-
-        JTextField ticketQuantityField = new JTextField(10);
-        JLabel ticketQuantityLabel = new JLabel("  Quantity");
-
-        JTextField ticketQuantityToBuy = new JTextField(10);
-        JLabel ticketQuantityToBuyLabel = new JLabel("Tickets to buy");
-
-        JTextField actualAvailabilityField = new JTextField(10);
-        JLabel actualAvailabilityFieldLabel = new JLabel("“Actual Availability”");
-
-        JButton buyBotton = new JButton("Buy");
-
-
-//        eventDetailPanel.setBorder(new EmptyBorder());
-        eventDetailPanel.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.fill = GridBagConstraints.VERTICAL;
-
-
-        eventDetailPanel.add(eventNameLabel);
-        eventDetailPanel.add(eventNameField, gbc);
-
-        eventDetailPanel.add(ticketQuantityLabel);
-        eventDetailPanel.add(ticketQuantityField, gbc);
-
-        eventDetailPanel.add(ticketQuantityToBuyLabel);
-        eventDetailPanel.add(ticketQuantityToBuy, gbc);
-
-        eventDetailPanel.add(actualAvailabilityFieldLabel);
-        eventDetailPanel.add(actualAvailabilityField, gbc);
-        eventDetailPanel.add(buyBotton, gbc);
-
-
 
         // Array für unsere JList
         String interessen[] = {"Politik", "Autos", "Mode",
@@ -135,8 +95,8 @@ public class ApplicationMain extends JPanel {
 
         //JList mit Einträgen wird erstellt
         JList themenAuswahl = new JList(listenModell);
-        for(int i=0; i<interessen.length; i++){
-            listenModell.addElement(interessen[i]);
+        for(String name : interessen){
+            listenModell.addElement(name);
         }
 
         //JList wird Panel hinzugefügt
@@ -145,28 +105,27 @@ public class ApplicationMain extends JPanel {
         scrollPaneForEvents.setViewportView(themenAuswahl);
         themenAuswahl.setLayoutOrientation(JList.VERTICAL);
         eventPanel.add(scrollPaneForEvents);
-        eventPanel.add(eventDetailPanel);
+        eventPanel.add(new EventPropertyUI());
         //Eintrag "Programmieren" wird an Index 4 eingefügt
         listenModell.add(4, "Programmieren");
 
         //erster Eintrag "Politik" wird entfernt
-        listenModell.removeElementAt(0);
+//        listenModell.removeElementAt(0);
 
         //String "Mode" wird entfernt
-        listenModell.removeElement("Mode");
+//        listenModell.removeElement("Mode");
 
 
 
-
-        tabbedPaneViewChooser.addTab("Tab 1", null, eventPanel,
-                "Does nothing");
+        tabbedPaneViewChooser.addTab("First view", null, eventPanel,
+                "Process to buy tickets");
 
         tabbedPaneViewChooser.setMnemonicAt(0, KeyEvent.VK_1);
 
 
 
-        tabbedPaneViewChooser.addTab("Tab 2", null, eventTablePanel,
-                "Does twice as much nothing");
+        tabbedPaneViewChooser.addTab("All records", null, eventTablePanel,
+                "Process to show table list");
         tabbedPaneViewChooser.setMnemonicAt(1, KeyEvent.VK_2);
 
 
@@ -178,14 +137,6 @@ public class ApplicationMain extends JPanel {
         tabbedPaneViewChooser.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
     }
 
-    protected JComponent makeTextPanel(String text) {
-        JPanel panel = new JPanel(false);
-        JLabel filler = new JLabel(text);
-        filler.setHorizontalAlignment(JLabel.CENTER);
-        panel.setLayout(new GridLayout(1, 1));
-        panel.add(filler);
-        return panel;
-    }
 
 
     /**
@@ -196,12 +147,12 @@ public class ApplicationMain extends JPanel {
     private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("ApplicationMain");
-        frame.setBounds(100, 100,1000,900);
+//        frame.setBounds(100, 100,2500,2000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        ApplicationMain applicationMain = new ApplicationMain();
-//        applicationMain.setPreferredSize((new Dimension(800,600)));
+        ApplicationMain applicationMain = new ApplicationMain();
+        applicationMain.setPreferredSize((new Dimension(800,400)));
         //Add content to the window.
-        frame.add(new ApplicationMain(), BorderLayout.CENTER);
+        frame.add(applicationMain, BorderLayout.CENTER);
 
         //Display the window.
         frame.pack();
@@ -219,17 +170,5 @@ public class ApplicationMain extends JPanel {
             }
         });
     }
-//    public static void main(String[] args) {
-//        JFrame f=new JFrame("Title");//creating instance of JFrame
-//
-//        JButton b=new JButton("click");//creating instance of JButton
-//        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
-//
-//        f.add(b);//adding button in JFrame
-//
-//        f.setSize(800,600);//400 width and 500 height
-//        f.setLayout(null);//using no layout managers
-//        f.setVisible(true);//making the frame visible
-//    }
 
 }
---
+++ src/main/java/de/thb/view/EventPropertyUI.java
@@ -0,0 +1,209 @@
+package de.thb.view;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import javax.swing.JSeparator;
+import javax.swing.SpringLayout;
+import javax.swing.SwingUtilities;
+import java.awt.Font;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+
+public class EventPropertyUI extends JPanel implements ActionListener, FocusListener {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    JTextField eventNameField, ticketQuantityField, ticketQuantityToBuyField;
+    boolean addressSet = false;
+    Font regularFont, italicFont;
+    JLabel actualAvailabilityDisplay;
+    final static int GAP_BETWEEN = 10;
+    final static int TEXTFIELD_COLUMN = 5;
+
+    public EventPropertyUI() {
+        setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+
+        JPanel leftHalf = new JPanel() {
+
+            private static final long serialVersionUID = 1L;
+
+            public Dimension getMaximumSize() {
+                Dimension pref = getPreferredSize();
+                return new Dimension(Integer.MAX_VALUE, pref.height);
+            }
+        };
+        leftHalf.setLayout(new BoxLayout(leftHalf, BoxLayout.PAGE_AXIS));
+        leftHalf.add(createEntryFields());
+        leftHalf.add(createButtons());
+
+        add(leftHalf);
+        add(createActualAvailabilityDisplay());
+    }
+
+    protected JComponent createButtons() {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+
+        JButton button = new JButton("Buy Tickets");
+        button.addActionListener(this);
+        panel.add(button);
+
+//        button = new JButton("Clear address");
+//        button.addActionListener(this);
+//        button.setActionCommand("clear");
+//        panel.add(button);
+
+        // Match the SpringLayout's gap, subtracting 5 to make
+        // up for the default gap FlowLayout provides.
+        panel.setBorder(BorderFactory.createEmptyBorder(0, 0, GAP_BETWEEN - 5, GAP_BETWEEN - 5));
+        return panel;
+    }
+
+    /**
+     * Called when the user clicks the button or presses Enter in a text field.
+     */
+    public void actionPerformed(ActionEvent e) {
+        if ("clear".equals(e.getActionCommand())) {
+            addressSet = false;
+            eventNameField.setText("");
+            ticketQuantityField.setText("");
+        } else {
+            addressSet = true;
+        }
+        updateDisplays();
+    }
+
+    protected void updateDisplays() {
+        actualAvailabilityDisplay.setText(getActualAvailability());
+        if (addressSet) {
+            actualAvailabilityDisplay.setFont(regularFont);
+        } else {
+            actualAvailabilityDisplay.setFont(italicFont);
+        }
+    }
+
+    protected JComponent createActualAvailabilityDisplay() {
+        JPanel panel = new JPanel(new BorderLayout());
+        actualAvailabilityDisplay = new JLabel();
+        actualAvailabilityDisplay.setHorizontalAlignment(JLabel.CENTER);
+        regularFont = actualAvailabilityDisplay.getFont().deriveFont(Font.BOLD, 16.0f);
+        italicFont = regularFont.deriveFont(Font.ITALIC);
+        updateDisplays();
+
+        // Lay out the panel.
+        panel.setBorder(BorderFactory.createEmptyBorder(GAP_BETWEEN / 2, // top
+                0, // left
+                GAP_BETWEEN / 2, // bottom
+                0)); // right
+        panel.add(new JSeparator(JSeparator.VERTICAL), BorderLayout.LINE_START);
+        panel.add(actualAvailabilityDisplay, BorderLayout.CENTER);
+        panel.setPreferredSize(new Dimension(200, 150));
+
+        return panel;
+    }
+
+    protected String getActualAvailability() {
+        if (!addressSet)
+            return "Actual Availability.";
+
+        String eventName = eventNameField.getText();
+        String ticketQuantity = ticketQuantityField.getText();
+        String ticketQuantityToBuy = ticketQuantityToBuyField.getText();
+        String empty = "";
+
+        if ((ticketQuantityToBuy == null) || empty.equals(ticketQuantityToBuy)) {
+            ticketQuantityToBuy = "<em>(There is no enough tickets for this event. Please select a number of Tickets)</em>";
+        }
+
+        return "<html><p align=center>" +" Actual Availability "+
+                eventName +
+                "<br>" +
+                "</p></html>";
+    }
+
+
+    /**
+     * Called when one of the fields gets the focus so that the
+     * focused field is selectable.
+     */
+    public void focusGained(FocusEvent e) {
+        Component c = e.getComponent();
+        if (c instanceof JTextField) {
+            selectItLater(c);
+        } else if (c instanceof JTextField) {
+            ((JTextField) c).selectAll();
+        }
+    }
+
+    // Workaround for formatted text field focus side effects.
+    protected void selectItLater(Component c) {
+        if (c instanceof JTextField) {
+            final JTextField ftf = (JTextField) c;
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    ftf.selectAll();
+                }
+            });
+        }
+    }
+
+    // Needed for FocusListener interface.
+    public void focusLost(FocusEvent e) {
+    } // ignore
+
+    protected JComponent createEntryFields() {
+        JPanel panel = new JPanel(new SpringLayout());
+
+        String[] labelStrings = { "Event: ", "Quantity: ", "Booked Tickets: "};
+
+        JLabel[] labels = new JLabel[labelStrings.length];
+        JComponent[] fields = new JComponent[labelStrings.length];
+        int fieldNum = 0;
+
+        // Create the text field and set it up.
+        eventNameField = new JTextField();
+        eventNameField.setColumns(TEXTFIELD_COLUMN);
+        fields[fieldNum++] = eventNameField;
+
+        ticketQuantityField = new JTextField();
+        ticketQuantityField.setColumns(TEXTFIELD_COLUMN);
+        fields[fieldNum++] = ticketQuantityField;
+
+        ticketQuantityToBuyField = new JTextField();
+        ticketQuantityField.setColumns(TEXTFIELD_COLUMN);
+        fields[fieldNum++] = ticketQuantityToBuyField;
+
+        // Associate label/field pairs, add everything,
+        // and lay it out.
+        for (int i = 0; i < labelStrings.length; i++) {
+            labels[i] = new JLabel(labelStrings[i], JLabel.TRAILING);
+            labels[i].setLabelFor(fields[i]);
+            panel.add(labels[i]);
+            panel.add(fields[i]);
+
+            // Add listeners to each field.
+            JTextField actualField = (JTextField) fields[i];
+
+            actualField.addActionListener(this);
+            actualField.addFocusListener(this);
+        }
+        EventUtilitiesUI.buildCompactGrid(
+                panel,
+                labelStrings.length,
+                2, 0, 0, 0, GAP_BETWEEN);
+        return panel;
+    }
+
+}
