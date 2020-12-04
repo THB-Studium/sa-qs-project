60eb9cf7ef78e22b9c4ea4d1601b3f391ab3e83c
Mistra Forest Kuipou Tchiendja <forest@cantaa.de>
2020-12-04 02:08:11 +0100

    Methode for alternate coloration added

--- src/main/java/de/thb/view/TableEventUI.java
+++ src/main/java/de/thb/view/TableEventUI.java
@@ -2,22 +2,22 @@ package de.thb.view;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.AdjustmentEvent;
-import java.awt.event.AdjustmentListener;
 
 public class TableEventUI extends JPanel {
 
     private static final int GAP_BETWEEN = 10;
     private static final int INITIAL_ROWS_NUMBER = 2;
-    private static String[] columnHeader = {
+    private static final String[] columnHeader = {
             "#",
             "Name",
             "Available tickets",
             "Booked tickets",
             "Actual availability"};
-    private DefaultTableModel tableModel = new DefaultTableModel(null, columnHeader) {
+    private final DefaultTableModel tableModel = new DefaultTableModel(null, columnHeader) {
 
         @Override
         public Class<?> getColumnClass(int col) {
@@ -25,28 +25,37 @@ public class TableEventUI extends JPanel {
         }
     };
 
-    private JTable table = new JTable(tableModel);
-    private JScrollPane scrollPane = new JScrollPane(table);
-    private JScrollBar vScroll = scrollPane.getVerticalScrollBar();
     private boolean isAutoScroll;
+    TableColumn tColumn;
 
     public TableEventUI(){
 
         //Table row dynamically added when an event is generated
         this.setLayout(new BorderLayout());
+        JTable table = new JTable(tableModel) {
+            public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+                Component returnComp = super.prepareRenderer(renderer, row, column);
+                Color alternateColor = Color.decode("#FFFAF0");
+                Color whiteColor = Color.WHITE;
+                if (!returnComp.getBackground().equals(getSelectionBackground())) {
+                    Color backgroundColor = (row % 2 == 0 ? alternateColor : whiteColor);
+                    returnComp.setBackground(backgroundColor);
+                }
+                this.getColumnModel().getColumn(4).setCellRenderer(EventUtilitiesUI.setColumnColor(Color.RED));
+                return returnComp;
+            }
+        };
         Dimension d = new Dimension(320, INITIAL_ROWS_NUMBER * table.getRowHeight());
         table.setPreferredScrollableViewportSize(d);
+        tColumn = table.getColumnModel().getColumn(4);
+        tColumn.setCellRenderer(EventUtilitiesUI.setColumnColor(Color.RED));
         for (int i = 0; i < INITIAL_ROWS_NUMBER; i++) {
             addRow();
         }
+        JScrollPane scrollPane = new JScrollPane(table);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        vScroll.addAdjustmentListener(new AdjustmentListener() {
-
-            @Override
-            public void adjustmentValueChanged(AdjustmentEvent e) {
-                isAutoScroll = !e.getValueIsAdjusting();
-            }
-        });
+        JScrollBar vScroll = scrollPane.getVerticalScrollBar();
+        vScroll.addAdjustmentListener(e -> isAutoScroll = !e.getValueIsAdjusting());
         this.add(scrollPane, BorderLayout.CENTER);
         JPanel panel = new JPanel(new GridLayout(1,2));
         panel.add(new JButton(new AbstractAction("Add Row") {
@@ -57,7 +66,7 @@ public class TableEventUI extends JPanel {
             }
         }));
         this.add(panel, BorderLayout.PAGE_END);
-        this.add(new Panel().add(EventUtilitiesUI.createButtons("Buy Tickets", GAP_BETWEEN-5)), BorderLayout.PAGE_END);
+        this.add(new Panel().add(EventUtilitiesUI.createButtons("Buy Tickets", GAP_BETWEEN-5)), BorderLayout.EAST);
     }
 
     private void addRow() {
