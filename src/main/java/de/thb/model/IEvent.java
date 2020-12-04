ed0f82022e74d7e28a9f3f52bb89ea2d8ca9e98e
diddy19 <56726689+damesse@users.noreply.github.com>
2020-12-04 21:37:32 +0100

    wip
    wip

--- src/main/java/de/thb/model/IEvent.java
+++ src/main/java/de/thb/model/IEvent.java
@@ -1,9 +1,8 @@
-import java.util.Date;
 package de.thb.model;
 
 public interface IEvent {
 
-	public void createEvent( String name, Date date, int numberOfTicket);
+	public void createEvent();
     public void updateEventNumberOfTicket(int numberOfTicket);
     
 }
