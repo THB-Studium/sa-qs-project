471abe2db61faef1fce428e81ae15990a21b4860
diddy19 <56726689+damesse@users.noreply.github.com>
2020-12-05 11:32:37 +0100

    new ConstantePercentage done
    new implementation of ConstantePercentage done, after deleting the last one because of conflict by pull request.

--- src/main/java/de/thb/model/ConstantPercentage.java
+++ src/main/java/de/thb/model/ConstantPercentage.java
@@ -1,4 +1,17 @@
 package de.thb.model;
 
 public enum ConstantPercentage {
+    PERCENTAGE_10(0.1f),
+    PERCENTAGE_5(0.05f),
+    PERCENTAGE_0(0.0f);
+
+    private float percentage;
+
+    ConstantPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
 }
